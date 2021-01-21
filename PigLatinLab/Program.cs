31376473128ac5d87f3b61d890b3e47b27943766
jwyeth <jwyeth@hotmail.com>
2021-01-21 15:30:46 -0500

    Initial commit

---
+++ PigLatinLab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PigLatinLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // apple -> appleway - vowel
+            // strong -> ongstray - constatant
+
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+            Console.Write("Enter a line to be translated: ");
+            string sentence = Console.ReadLine();
+         }
+
+        static string ConvertToPigLatin(string sentence)
+        {
+
+        }
+
+    }
+}
