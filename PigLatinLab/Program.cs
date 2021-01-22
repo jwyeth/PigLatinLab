a193182d8c348648de7ea1eae8bfdd586643a7ea
jwyeth <jwyeth@hotmail.com>
2021-01-22 13:45:19 -0500

    Functioning Pig Latin Translation

--- PigLatinLab/Program.cs
+++ PigLatinLab/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PigLatinLab
 {
@@ -12,12 +13,80 @@ namespace PigLatinLab
             Console.WriteLine("Welcome to the Pig Latin Translator!");
             Console.Write("Enter a line to be translated: ");
             string sentence = Console.ReadLine();
-         }
+            string lowerSentence = sentence.ToLower();
+            //Console.Write(lowerSentence);
 
-        static string ConvertToPigLatin(string sentence)
+            //string[] newWords = sentence.Split();
+            //int length = newWords.Length;
+
+            //TranslateToPigLatin(lowerSentence);
+
+            const string vowels = "aeiou";
+            foreach (string word in lowerSentence.Split())
+            {
+                string firstLetter = word.Substring(0, 1);
+                string restOfWord = word.Substring(1, word.Length - 1);
+                int currentLetter = vowels.IndexOf(firstLetter);
+
+
+
+                //string[] newWords = new string[word.Length];
+
+                //Console.WriteLine(firstLetter);
+                //Console.WriteLine(restOfWord);
+                //Console.WriteLine(currentLetter);
+
+                var newWords = "";
+
+                if (currentLetter == -1)
+                {
+                    newWords += restOfWord + firstLetter + "ay";
+                }
+                else
+                {
+                    newWords += word + "way ";
+                }
+
+                Console.WriteLine(newWords);
+
+                /*if (currentLetter == -1)
+                {
+                    new
+                }*/
+
+
+            }
+
+        /*public static string TranslateToPigLatin(string lowerSentence)
         {
+            const string vowels = "aeiou";
+            foreach (string word in lowerSentence.Split())
+            {
+                string firstLetter = word.Substring(0, 1);
+                string restOfWord = word.Substring(1, word.Length - 1);
+                int currentLetter = vowels.IndexOf(firstLetter);
 
-        }
+                Console.WriteLine(firstLetter);
+                Console.WriteLine(restOfWord);
+                Console.WriteLine(currentLetter);
+
+                if (currentLetter == -1)
+                {
+                    new
+                }
+
+                return firstLetter;
+                
+
+
+            }*/
+
+    } 
+
+
+
+
+
 
     }
 }
